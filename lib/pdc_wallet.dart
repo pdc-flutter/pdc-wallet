f53f1a557c79122477b904991faa6c05d0e2e49a
johirul islam <ragislam@gmail.com>
2021-06-28 23:50:27 +0600

    first commit

---
+++ lib/pdc_wallet.dart
@@ -0,0 +1,15 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library pdc_wallet;
+
+export 'src/pdc_wallet_base.dart';
+export 'src/models/networks.dart';
+export 'src/transaction.dart';
+export 'src/address.dart';
+export 'src/transaction_builder.dart';
+export 'src/ecpair.dart';
+export 'src/payments/p2pkh.dart';
+export 'src/payments/p2wpkh.dart';
+export 'src/payments/index.dart';
+// TODO: Export any libraries intended for clients of this package.
---
+++ lib/src/models/networks.dart
@@ -0,0 +1,67 @@
+import 'package:meta/meta.dart';
+
+class NetworkType {
+  String messagePrefix;
+  String bech32;
+  Bip32Type bip32;
+  int pubKeyHash;
+  int scriptHash;
+  int wif;
+
+  NetworkType(
+      {@required this.messagePrefix,
+      this.bech32,
+      @required this.bip32,
+      @required this.pubKeyHash,
+      @required this.scriptHash,
+      @required this.wif});
+
+  @override
+  String toString() {
+    return 'NetworkType{messagePrefix: $messagePrefix, bech32: $bech32, bip32: ${bip32.toString()}, pubKeyHash: $pubKeyHash, scriptHash: $scriptHash, wif: $wif}';
+  }
+}
+
+class Bip32Type {
+  int public;
+  int private;
+
+  Bip32Type({@required this.public, @required this.private});
+
+  @override
+  String toString() {
+    return 'Bip32Type{public: $public, private: $private}';
+  }
+}
+
+final pandora = new NetworkType(
+    messagePrefix: '\x18Pandora Signed Message:\n',
+    bech32: 'pdc',
+    bip32: new Bip32Type(public: 0x0488b21e, private: 0x0488ade4),
+    pubKeyHash: 0x00,
+    scriptHash: 0x05,
+    wif: 0x80);
+
+final pdctestnet = new NetworkType(
+    messagePrefix: '\x18Pandora Signed Message:\n',
+    bech32: 'tpdc',
+    bip32: new Bip32Type(public: 0x043587cf, private: 0x04358394),
+    pubKeyHash: 0x6f,
+    scriptHash: 0xc4,
+    wif: 0xef);
+
+final bitcoin = new NetworkType(
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'bc',
+    bip32: new Bip32Type(public: 0x0488b21e, private: 0x0488ade4),
+    pubKeyHash: 0x00,
+    scriptHash: 0x05,
+    wif: 0x80);
+
+final testnet = new NetworkType(
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'tb',
+    bip32: new Bip32Type(public: 0x043587cf, private: 0x04358394),
+    pubKeyHash: 0x6f,
+    scriptHash: 0xc4,
+    wif: 0xef);
